8df7800bf95993ab69ed4258e64440bc629a9743
Anum Baig <anumsohail2522@gmail.com>
2025-09-15 13:16:35 +0500

    Create Task1.cpp

---
+++ Lab 05/Task1.cpp
@@ -0,0 +1,17 @@
+# include <iostream>
+using namespace std;
+
+int calculateFactorial(int n){
+    if(n == 0 ||n == 1){
+        return 1;
+    }
+    return n * calculateFactorial(n-1);
+}
+
+int main(){
+    int n;
+    cout << "Enter number: " << endl;
+    cin >> n;
+
+    cout << "Factorial of " << n << ": " << calculateFactorial(n) << endl;
+}
